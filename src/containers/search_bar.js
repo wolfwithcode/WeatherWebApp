f5d87ca32261d99d0ea5dc83f5551d6ce1679253
Zachary Reece <zachdreece@gmail.com>
2018-01-03 22:13:20 -0600

    Changing git name

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,7 +4,7 @@ export default class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: '' }
+    this.state = { term: 'help' }
   }
 
   onInputChange(event) {
